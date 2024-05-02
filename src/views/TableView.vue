770cee3eed8854f1bb2f88ea02230e9e7e196922
Catman06 <zippercat06@gmail.com>
2024-05-02 11:25:38 -0500

    Base placeholder table created pt2

--- src/views/TableView.vue
+++ src/views/TableView.vue
@@ -3,20 +3,60 @@ import TableItem from '../components/TableItem.vue'
 </script>
 
 <template>
-    <div class="table">
-      <h1>This will be where the table is to be put</h1>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Header1</th>
+          <th>Header2</th>
+          <th>Header3</th>
+        </tr> 
+      </thead>
+
       <TableItem />
-    </div>
+      <TableItem />
+      <TableItem />
+      <TableItem />
+      <TableItem />
+    </table>
   </template>
   
   <style>
+  /* Table */
   .table {
+    --table-border-style: solid;
+    --table-border-width: 3px;
+    --table-border-color: var(--accent-green);
+
     text-align: center;
+    width: 100%;
+    margin-top: 2rem;
+    border-radius: 10px;
+    border-spacing: 0;
+    border: var(--table-border-width) var(--table-border-style) var(--table-border-color);
   }
+
+  thead {
+    background-color: var(--accent-green);
+  }
+
+  /* TableItems are classed 'item' */
+  .item {
+    background-color: var(--accent-grey);
+  }
+  .item:nth-of-type(even) {
+    background-color: var(--accent-light-grey);
+  }
+  @media (hover: hover) {
+  .item:hover {
+    background-color: var(--item-hover);
+  }
+  .item:active {
+    background-color: var(--item-active);
+  }
+}
+
+  /* Horizontal Display */
   @media (min-width: 1024px) {
-    .table {
-      min-height: 100vh;
-      display: flex;
-    }
+
   }
   </style>
